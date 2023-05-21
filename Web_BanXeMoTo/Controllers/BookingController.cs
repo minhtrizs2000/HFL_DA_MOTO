ed23739a0776b1a45ea9893fdf76004c8d140ce4
minhtrizs2000 <minhtrizs2000@gmail.com>
2023-05-22 00:09:18 +0700

    reup project

---
+++ Web_BanXeMoTo/Controllers/BookingController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_BanXeMoTo.Models;
+
+
+namespace Web_BanXeMoTo.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly QLMoToContext database;
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public BookingController(QLMoToContext db)
+        {
+            database = db;
+            this.hostEnvironment = hostEnvironment;
+        }
+        public IActionResult BookingList(/*int id*/)
+        {
+            //var email =HttpContext.Session.GetString("email");
+
+            var model = new ViewModelCus();
+            model.ListDatLich = database.DatLiches.ToArray();
+            model.ListHang = database.Hangs.ToArray();
+            model.ListMauXe = database.MauXes.ToArray();
+            model.ListKhachHang = database.KhachHangs/*.Where(x=>x.Idkh==id)*/.ToArray();
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            if (HttpContext.Session.GetString("email") == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var model = new ViewModelCus();
+            model.ListHang = database.Hangs.ToArray();
+            model.ListMauXe = database.MauXes.ToArray();
+            model.ListKhachHang = database.KhachHangs.ToArray();
+            return View(model);
+            //return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(DatLich datlich)
+        {
+            var model = new ViewModelCus();
+            model.ListHang = database.Hangs.ToArray();
+            model.ListMauXe = database.MauXes.ToArray();
+            model.ListKhachHang = database.KhachHangs.ToArray();
+            if (ModelState.IsValid)
+            {
+                database.Add(datlich);
+                await database.SaveChangesAsync();
+                return RedirectToAction("Products", "Products");
+            }
+            return View(model);
+        }
+        public IActionResult Delete(int id)
+        {
+            var model = new ViewModelCus();
+            model.ListHang = database.Hangs.ToArray();
+            model.ListMauXe = database.MauXes.ToArray();
+            model.ListKhachHang = database.KhachHangs.ToArray();
+            model.datLich = database.DatLiches.Where(x => x.IddatLich == id).FirstOrDefault();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id, DatLich datLich)
+        {
+            datLich = database.DatLiches.Where(x => x.IddatLich == id).FirstOrDefault();
+            database.Remove(datLich);
+            database.SaveChanges();
+            return RedirectToAction("BookingList");
+        }
+        public IActionResult Edit(int id)
+        {
+
+            var model = new ViewModelCus();
+            model.ListDatLich = database.DatLiches.ToArray();
+            model.ListHang = database.Hangs.ToArray();
+            model.ListMauXe = database.MauXes.ToArray();
+            model.ListKhachHang = database.KhachHangs.ToArray();
+            model.datLich = database.DatLiches.Where(x => x.IddatLich == id).FirstOrDefault();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, DatLich datLich)
+        {
+            var model = new ViewModelCus();
+            model.ListHang = database.Hangs.ToArray();
+            model.ListMauXe = database.MauXes.ToArray();
+            model.ListKhachHang = database.KhachHangs.ToArray();
+            model.datLich = database.DatLiches.Where(x => x.IddatLich == id).FirstOrDefault();
+            if (datLich.Idmau != null && datLich.Idkh != null && datLich.NgayDat > DateTime.Now)
+            {
+                model.datLich.Idmau = datLich.Idmau;
+                model.datLich.Idkh = datLich.Idkh;
+                model.datLich.NgayDat = datLich.NgayDat;
+                model.datLich.GhiChu = datLich.GhiChu;
+
+                database.Update(model.datLich);
+                await database.SaveChangesAsync();
+                return RedirectToAction("BookingList", "Booking");
+            }
+
+            return View(model);
+        }
+
+        //public ActionResult SelectMau()
+        //{
+        //    var model = new ViewModel();
+        //    model.ListHang = database.Hangs.ToArray();
+        //    model.ListMauXe = database.MauXes.ToArray();
+        //    return View(model);
+
+        //}
+    }
+}
---
+++ Web_BanXeMoTo/Controllers/HangController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_BanXeMoTo.Models;
+using X.PagedList;
+
+namespace Web_BanXeMoTo.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class HangController : Controller
+    {
+        private readonly QLMoToContext database;
+
+        public HangController(QLMoToContext db)
+        {
+            database = db;
+        }
+        
+        public IActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            //A ViewBag property provides the view with the current sort order, because this must be included in 
+            //  the paging links in order to keep the sort order the same while paging
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            ViewBag.Role = TempData["Role"];
+
+            var ModelList = new List<Hang>();
+
+            //ViewBag.CurrentFilter, provides the view with the current filter string.
+            //he search string is changed when a value is entered in the text box and the submit button is pressed. In that case, the searchString parameter is not null.
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+            using (var context = new QLMoToContext())
+            {
+                var model = from s in context.Hangs
+                            select s;
+                //Search and match data, if search string is not null or empty
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    model = model.Where(s => s.Idhang.Contains(searchString)
+                                           || s.Idhang.Contains(searchString)
+                                           || s.TenHang.Contains(searchString));
+                }
+                switch (sortOrder)
+                {
+                    case "name_desc":
+                        ModelList = model.OrderByDescending(s => s.Idhang).ToList();
+                        break;
+
+                    default:
+                        ModelList = model.OrderBy(s => s.Idhang).ToList();
+                        break;
+                }
+
+            }
+            //indicates the size of list
+            int pageSize = 10;
+            //set page to one is there is no value, ??  is called the null-coalescing operator.
+            int pageNumber = (page ?? 1);
+            //return the Model data with paged
+            var modelv = new ViewModel
+            {
+                ListHangs = ModelList.ToPagedList(pageNumber, pageSize),
+                ListMauXe = database.MauXes.ToArray(),
+                
+            };
+            return View(modelv);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Role = TempData["Role"];
+
+            var model = new Hang();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Hang hang)
+        {
+            if (ModelState.IsValid)
+            {
+            database.Add(hang);
+            database.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(hang);
+        }
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            var model = new ViewModel();
+            model.hang = database.Hangs.Where(x => x.Idhang == id).FirstOrDefault();
+            database.Remove(model.hang);
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(string id)
+        {
+            ViewBag.Role = TempData["Role"];
+
+            var model = new Hang();
+            model = database.Hangs.Where(x => x.Idhang == id).FirstOrDefault();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Hang hang)
+        {
+            //foreach (var item in model.ListMauXe)
+            //{
+            //    if(item.Idhang == hang.Idhang)
+            //    {
+            //        //
+            //    }
+            //}
+            var model = hang;
+            if (ModelState.IsValid)
+            {
+                database.Update(model);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
